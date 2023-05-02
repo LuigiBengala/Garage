7208f27b6438b2b520878a3f0055f69aaf04e6a8
LuigiBengala <121065602+LuigiBengala@users.noreply.github.com>
2023-05-03 00:14:59 +0100

    Adiciona carros  e mostra carros

--- Garagem_prot1/funcoes.py
+++ Garagem_prot1/funcoes.py
@@ -1,6 +1,5 @@
 import os 
 import classes
-import ui
 import sqlite3
 
 lista_carros = list()
@@ -15,3 +14,65 @@ def pressione_enter():
     input("Pressione ENTER para continuar...")
     
 # ----------- CARROS -----------
+
+def add_carros():
+    
+    limpar_consola()
+    
+    global conn
+    conn = sqlite3.connect('garagem.db')
+    
+    print("Insira os dados do carro: ")
+    marca = input("Marca: ")
+    modelo = input("Modelo: ")
+    ano = int(input("Ano: "))
+    combustivel = input("Combustivel: ")
+    cilindrada = float(input("Cilindrada: "))
+    potencia = input("Potencia: ")
+    num_lugares = int(input("Numero de lugares: "))
+    
+    sql = '''
+        INSERT INTO carros(marca, modelo, ano, combustivel, cilindrada, potencia, num_lugares) VALUES(?,?,?,?,?,?,?);
+    '''
+    
+    c = conn.cursor()
+    
+    try:
+        c.execute(sql, (marca, modelo, ano, combustivel, cilindrada, potencia, num_lugares,))
+        conn.commit()
+        print("Carro adicionado com sucesso!")
+        pressione_enter()
+    except Exception as e:
+        print("Erro: ", e)
+        pressione_enter()
+        
+    limpar_consola()
+
+def show_cars():
+    
+    limpar_consola()
+    
+    global conn
+    conn = sqlite3.connect('garagem.db')
+    
+    sql = '''
+        SELECT id, marca, modelo, ano, combustivel, cilindrada, potencia, num_lugares FROM carros;
+    '''
+    
+    c = conn.execute(sql)
+    
+    print("# ----- CARROS ----- #")
+    print(" ")
+    
+    for dados in c:
+        print("ID: ", dados[0])
+        print("Marca: ", dados[1])
+        print("Modelo: ", dados[2])
+        print("Ano: ", dados[3])
+        print("Combustivel: ", dados[4])
+        print("Cilindrada: ", dados[5])
+        print("Potencia: ", dados[6])
+        print("Numero de lugares: ", dados[7])
+        print(" ")
+        print("-------------------------------")
+        print(" ")
--- Garagem_prot1/tables.py
+++ Garagem_prot1/tables.py
@@ -7,7 +7,7 @@ def criar_base_dados():
     conn = sqlite3.connect('garagem.db')
     
     sql_carros = '''
-        CREATE TABLE IF NOT EXISTS carros(
+        CREATE TABLE IF NOT EXISTS carros (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         marca text NOT NULL,
         modelo text NOT NULL,
@@ -15,7 +15,25 @@ def criar_base_dados():
         combustivel text NOT NULL,
         cilindrada float NOT NULL,
         potencia text NOT NULL,
-        num_lugares integer NOT NULL,
-        
+        num_lugares integer NOT NULL
         ); 
     '''
+    
+    sql_condutores = '''
+        CREATE TABLE IF NOT EXISTS condutores (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nome text NOT NULL,
+        tipo_de_carta text NOT NULL,
+        anos_de_carta integer NOT NULL                         
+        );
+    '''
+    
+    try:
+        conn.executescript(sql_carros)
+        conn.executescript(sql_condutores)
+        conn.commit()
+        print('Base de dados criada com sucesso!')
+        pressione_enter()
+    except Exception as e:
+        print('Erro: ', e)
+        pressione_enter()
